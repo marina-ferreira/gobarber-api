73359010c7efe564d80ad780818ff7b95280a6d4
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-23 07:42:07 -0300

    Node debugging

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,17 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello there!' })
+})
+
+app.post('/users', (request, response) => {
+  const { name, email } = request.body
+  const user = { name, email }
+
+  return response.json({ user })
 })
 
 app.listen(4000, () => console.log('🍀Server started on port 4000'))
