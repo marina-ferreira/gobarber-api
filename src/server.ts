9e46d526070530be2fe0697dc2fd763d80755230
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-23 08:28:23 -0300

    Create appointments endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello there!' })
-})
-
-app.post('/users', (request, response) => {
-  const { name, email } = request.body
-  const user = { name, email }
-
-  return response.json({ user })
-})
+app.use(routes)
 
 app.listen(4000, () => console.log('🍀Server started on port 4000'))
