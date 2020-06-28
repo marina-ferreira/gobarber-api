459f7e778c53679b973d57d54aee700ec85f280c
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-28 07:35:14 -0300

    Updates appointment route/service to get provider id

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -5,12 +5,12 @@ import Appointment from '../models/Appointment'
 import AppointmentRepository from '../repositories/AppointmentRepository'
 
 interface Request {
-  provider: string,
+  provider_id: string,
   date: Date
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentRepository = getCustomRepository(AppointmentRepository)
     const appointmentDate = startOfHour(date)
     const findAppointment = await appointmentRepository.findByDate(appointmentDate)
@@ -20,7 +20,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate
     })
 
