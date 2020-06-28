e5ebbb739a543d517df4727e39925275aa97c60f
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-28 19:54:57 -0300

    Global error handling

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,21 +5,17 @@ import AuthenticateUserService from '../services/AuthenticateUserService'
 const sessionsRouter = Router()
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body
-    const authenticateUserService = new AuthenticateUserService()
+  const { email, password } = request.body
+  const authenticateUserService = new AuthenticateUserService()
 
-    const { user, token } = await authenticateUserService.execute({
-      email,
-      password
-    })
+  const { user, token } = await authenticateUserService.execute({
+    email,
+    password
+  })
 
-    delete user.password
+  delete user.password
 
-    response.json({ user, token })
-  } catch (error) {
-    return response.status(error.statusCode).json({ error: error.message })
-  }
+  response.json({ user, token })
 })
 
 export default sessionsRouter
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata'
 
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+
 import routes from './routes'
 import uploadConfig from './config/upload'
+import AppError from './errors/AppError'
 
 import './database'
 
@@ -11,5 +14,18 @@ const app = express()
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
 
 app.listen(4000, () => console.log('🍀Server started on port 4000'))
