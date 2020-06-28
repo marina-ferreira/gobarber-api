34849dfe041b65532068d71787b63500502f0ea2
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-28 06:49:27 -0300

    Add provider id to appointments table migration

--- src/database/migrations/1593168917628-CreateAppointments.ts
+++ src/database/migrations/1593168917628-CreateAppointments.ts
@@ -8,7 +8,7 @@ export default class CreateAppointments1593168917628 implements MigrationInterfa
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
