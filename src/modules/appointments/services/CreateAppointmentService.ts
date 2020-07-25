36375e928c9706666a09853f037b709b197e99e7
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-25 15:47:18 -0300

    Migrates repositories to DDD

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -2,7 +2,7 @@ import { startOfHour } from 'date-fns'
 import { getCustomRepository } from 'typeorm'
 
 import Appointment from '../../../models/Appointment'
-import AppointmentRepository from '../../../repositories/AppointmentRepository'
+import AppointmentRepository from '../repositories/AppointmentRepository'
 import AppError from '../../../errors/AppError'
 
 interface Request {
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { parseISO } from 'date-fns'
 import { getCustomRepository } from 'typeorm'
 
-import AppointmentRepository from '../repositories/AppointmentRepository'
+import AppointmentRepository from '../modules/appointments/repositories/AppointmentRepository'
 import CreateAppointmentService from '../modules/appointments/services/CreateAppointmentService'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
