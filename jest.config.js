b42a5d0c25e531123db3502b3b795d230e1f1a0c
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-26 12:43:39 -0300

    Jest coverage config

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,15 @@ const { compilerOptions } = require('./tsconfig.json')
 
 module.exports = {
   clearMocks: true,
-  coverageProvider: "v8",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/modules/**/services/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'lcov'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
   preset: 'ts-jest',
-  testEnvironment: "node",
-  testMatch: ["**/*.spec.ts"]
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts']
 }
