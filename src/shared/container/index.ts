72652d3f3119f3c6e9cde1b84fb7fc747b389f2e
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-08-01 14:28:52 -0300

    Routes params validation with celebrate

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -15,9 +15,9 @@ import UserTokensRepository from '@modules/users/infra/typeorm/repositories/User
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository'
 import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository'
 
-container.registerSingleton<IAppointmentsRepository>(
-  'AppointmentsRepository',
-  AppointmentsRepository
+container.registerSingleton<INotificationsRepository>(
+  'NotificationsRepository',
+  NotificationsRepository
 )
 
 container.registerSingleton<IUsersRepository>(
@@ -30,7 +30,7 @@ container.registerSingleton<IUserTokensRepository>(
   UserTokensRepository
 )
 
-container.registerSingleton<INotificationsRepository>(
-  'NotificationsRepository',
-  NotificationsRepository
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository
 )
