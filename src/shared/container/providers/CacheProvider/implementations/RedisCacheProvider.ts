ea6d79602dc09e8ee30eff80d5bcb5d474bebba4
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-08-01 20:59:40 -0300

    Redis invalidate prefix

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -19,13 +19,21 @@ class RedisCacheProvider implements ICacheProvider {
 
   public async recover<T>(key: string): Promise<T | null> {
     const data = await this.client.get(key)
-    console.log(data)
     if (!data) return null
 
     return JSON.parse(data) as T
   }
 
   public async invalidate(key: string): Promise<void> {}
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = await this.client.keys(`${prefix}:*`)
+    const pipeline = this.client.pipeline()
+
+    keys.forEach(key => pipeline.del(key))
+
+    await pipeline.exec()
+  }
 }
 
 export default RedisCacheProvider
