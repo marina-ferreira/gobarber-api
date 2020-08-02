310130f182a757a5074b35e26a700fcd4129e2f6
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-08-02 17:46:42 -0300

    Appoinments repository user eager loading

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -54,7 +54,8 @@ class AppointmentsRepository implements IAppointmentsRepository {
           dateFieldName =>
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`
         )
-      }
+      },
+      relations: ['user']
     })
   }
 
