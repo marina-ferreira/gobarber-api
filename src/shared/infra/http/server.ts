88dbc70a505587023db039d935f8a12467c4911b
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-08-01 23:50:24 -0300

    Api limit rate

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import 'express-async-errors'
 
 import routes from '@shared/infra/http/routes'
 import uploadConfig from '@config/upload'
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter'
 import AppError from '@shared/errors/AppError'
 
 import '@shared/infra/typeorm'
@@ -15,6 +16,7 @@ import '@shared/container'
 
 const app = express()
 
+app.use(rateLimiter)
 app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder))
